5b21f0a78475390b9e0c7a7d86e191bd8931dafc
Ghannoum99 <ghannoumjihad59@gmail.com>
2021-08-13 20:07:04 +0200

    Adding footer

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -19,7 +19,7 @@ def index(request):
             data = {
                 "country_code": str(list_of_data['sys']['country']),
                 "coordinate": str(list_of_data['coord']['lon']) + ',' +str(list_of_data['coord']['lat']),
-                "temp": str(list_of_data['main']['temp']) + '°',
+                "temp": str(list_of_data['main']['temp']) ,
                 "pressure": str(list_of_data['main']['pressure']),
                 "humidity": str(list_of_data['main']['humidity']),
                 "main": str(list_of_data['weather'][0]['main']),
